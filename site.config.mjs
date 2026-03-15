6562672ee3a86ca5d62f86001b22aa5e4e885edc
Paul <paul@theapplegates.com>
2026-03-14 20:46:32 -0400

    test

--- site.config.mjs
+++ site.config.mjs
@@ -11,9 +11,9 @@ if (!hasSiteUrl && process.env.NODE_ENV === 'production') {
 
 export const site = {
   url: hasSiteUrl ? siteUrl : fallbackSiteUrl,
-  title: 'Astro Themes by Whono',
-  brandTitle: 'Whono',
-  author: 'Whono',
+  title: 'Living Life',
+  brandTitle: 'Lifing Life',
+  author: 'Paul Applegate',
   authorAvatar: 'author/avatar.webp',
   description: '一个 Astro 主题的展示站：轻量、可维护、可复用。'
 };
